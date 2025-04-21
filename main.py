f12b3a639e04339e4b11730a31f1c454c33dae49
Han Yixuan <77487009+Exuan148@users.noreply.github.com>
2025-04-21 17:52:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
 import time
-
+import argparse
 X = np.array([[1], [2], [3],[10],[21]])
 y = np.array([2, 4, 6,33,38])
 X2 = np.array([[0.1], [2], [3],[10],[21]])
@@ -9,6 +9,9 @@ y2 = np.array([2, 12, 6,30,38])
 #线性回归
 model = LinearRegression().fit(X, y)
 model2 = LinearRegression().fit(X2, y2)
-
-time.sleep(5)
+parser = argparse.ArgumentParser()
+parser.add_argument("--arg1")
+parser.add_argument("--arg2")
+args = parser.parse_args()
+print(args.arg1, args.arg2)
 print("模型系数:", model.coef_,model2.coef_)
