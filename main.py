163050b499f61b6a09ee7d8fa80bbdb699d0697d
Han Yixuan <77487009+Exuan148@users.noreply.github.com>
2025-04-21 17:13:38 +0800

    Revert "Update main.py"

--- main.py
+++ main.py
@@ -3,12 +3,9 @@ from sklearn.linear_model import LinearRegression
 import time
 
 X = np.array([[1], [2], [3],[10],[21]])
+
 y = np.array([2, 4, 6,33,38])
-X2 = np.array([[0.1], [2], [3],[10],[21]])
-y2 = np.array([2, 12, 6,30,38])
 #线性回归
 model = LinearRegression().fit(X, y)
-model2 = LinearRegression().fit(X2, y2)
-
 time.sleep(5)
-print("模型系数:", model.coef_,model2.coef_)
+print("模型系数:", model.coef_)
